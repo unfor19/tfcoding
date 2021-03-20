d5e5fd735022c6e4bff77f97976f227984dbe245
Meir Gabay <unfor19@gmail.com>
2021-03-20 16:50:11 +0200

    added app

---
+++ Dockerfile
@@ -0,0 +1,37 @@
+ARG TERRAFORM_VERSION="0.13.5"
+ARG HCL2JSON_VERSION="v0.3.2"
+ARG APP_USER_NAME="appuser"
+ARG APP_USER_ID="1000"
+ARG APP_GROUP_NAME="appgroup"
+ARG APP_GROUP_ID="1000"
+
+FROM alpine:3.12 as download
+ARG TERRAFORM_VERSION
+ARG HCL2JSON_VERSION
+
+WORKDIR /downloads/
+RUN apk add --no-cache unzip curl
+RUN curl -sL -o terraform.zip "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
+RUN unzip terraform.zip && rm terraform.zip
+RUN curl -sL -o hcl2json "https://github.com/tmccombs/hcl2json/releases/download/${HCL2JSON_VERSION}/hcl2json_linux_amd64" && chmod +x hcl2json
+
+
+FROM alpine:3.13 as app
+ARG APP_USER_NAME="appuser"
+ARG APP_USER_ID="1000"
+ARG APP_GROUP_NAME="appgroup"
+ARG APP_GROUP_ID="1000"
+
+RUN apk add --no-cache bash jq perl
+COPY --from=download /downloads/terraform /usr/local/bin/terraform
+COPY --from=download /downloads/hcl2json /usr/local/bin/hcl2json
+
+WORKDIR /code/
+RUN \
+    addgroup -g "${APP_GROUP_ID}" "${APP_GROUP_NAME}" && \
+    adduser -H -D -u "$APP_USER_ID" -G "$APP_GROUP_NAME" "$APP_USER_NAME" && \
+    chown -R "$APP_USER_ID":"$APP_GROUP_ID" .
+USER "$APP_USER_NAME"
+
+COPY entrypoint.sh .
+ENTRYPOINT ["bash", "/code/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+# Global variables
+_CODE_FILE_NAME="tfcoding.tf"
+_DEBUG=${DEBUG:-"false"}
+_SRC_DIR_ROOT="/src"
+_SRC_DIR_RELATIVE_PATH="${1:-"$RELATIVE_PATH"}"
+_SRC_DIR_RELATIVE_PATH="${_SRC_DIR_RELATIVE_PATH:-"$_ROOT_DIR"}"
+_SRC_DIR_ABSOLUTE_PATH="${_SRC_DIR_ROOT}/${_SRC_DIR_RELATIVE_PATH}"
+_CODE_DIR_ROOT="/code"
+
+
+# Functions
+error_msg(){
+    local msg=$1
+    echo -e "[ERROR] $msg"
+    exit 1
+}
+
+
+copy_files(){
+  # Copy relevant files from /src/ to /code/ dir
+  rm -rf /code/*
+  find "$_SRC_DIR_ABSOLUTE_PATH" -type f \( -name ''"$_CODE_FILE_NAME"'' -o -name '*.tpl' -o -name '*.json' \) \
+    -and \( -not -path '*.git/*' -not -path '*.terraform/*' -and -not -path ''"$_CODE_DIR_ROOT"'*' \) -exec cp {} ${_CODE_DIR_ROOT} \;
+  
+  if [[ ! -f "$_CODE_FILE_NAME" ]]; then
+    error_msg "File does not exist - $_CODE_FILE_NAME"
+  fi
+}
+
+
+terraform_init(){
+  # Create a local empty tfstate file
+  terraform init 1>/dev/null
+}
+
+
+inject_outputs(){
+  # Inject outputs to $_CODE_FILE_NAME according to locals{}
+  declare -a arr=($(hcl2json "$_CODE_FILE_NAME" | jq -r '.locals[] | keys[]'))
+  for local_value in "${arr[@]}"; do
+      cat <<EOF >> "$_CODE_FILE_NAME"
+output "${local_value}" {
+    value = local.${local_value}
+}
+EOF
+  done
+}
+
+
+debug_mode(){
+  if [[ $_DEBUG = "true" ]]; then
+    cat "$_CODE_FILE_NAME"
+  fi
+}
+
+
+render_tfcoding(){
+  # terraform apply renders the outputs
+  # perl removes all strings from start to "Outputs:"
+  # hcl2json | jq - pretty formatting
+  terraform apply -auto-approve -no-color | perl -p0e 's/.*Outputs:.*\n\n//s' | hcl2json | jq
+}
+
+
+# Main
+copy_files
+terraform_init
+inject_outputs
+debug_mode
+render_tfcoding
