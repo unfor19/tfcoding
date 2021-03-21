4d94fc2e63a7930b94f9674e135cbb8a76a0a770
Meir Gabay <unfor19@gmail.com>
2021-03-22 01:49:56 +0200

    added colorful tests

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -7,7 +7,7 @@ _DOCKERHUB_TAG="${DOCKERHUB_TAG:-"unfor19/tfcoding:latest"}"
 
 error_msg(){
     local msg=$1
-    echo -e "[ERROR] $msg"
+    echo -e "\e[31m[ERROR]\e[0m $msg"
     export DEBUG=1
     exit 1
 }
@@ -25,9 +25,9 @@ should(){
     echo -e "[LOG] Output:\n\n$output_msg\n"
 
     if [[ $expected == "pass" && $output_code -eq 0 && ! $output_msg =~ .*(ERROR|Error|error).* ]]; then
-        echo "[LOG] Test passed as expected"
+        echo -e "\e[92m[SUCCESS]\e[0m Test passed as expected"
     elif [[ $expected == "fail" && $output_code -eq 1 ]] || [[ $expected == "fail" && $output_msg =~ .*(ERROR|Error|error).* ]]; then
-        echo "[LOG] Test failed as expected"
+        echo -e "\e[92m[SUCCESS]\e[0m Test failed as expected"
     else
         error_msg "Test output is not expected, terminating"
     fi
@@ -46,6 +46,6 @@ should pass "Basic Example - All local values" "tfcoding -r examples/basic"
 should pass "Basic Example - Single local value" "tfcoding -r examples/basic -o private_subnets"
 should pass "Complex Example - All local values" "tfcoding -r examples/complex"
 should pass "Complex Example - Single local values" "tfcoding -r examples/complex -o my_keys"
+should pass "No arguments provided - Scans root src dir for tfcoding.tf, uses the last file that was found" "tfcoding"
 should fail "Unknown single value" "tfcoding -r examples/complex -o unknown_local_value"
-should fail "No arguments provided" "tfcoding"
 should fail "Non existing dir" "tfcoding -r examples/non-existing-dir"
